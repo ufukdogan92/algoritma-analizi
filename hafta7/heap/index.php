b46ae58a1dd2b9a84ef9c3e948ac89fdf9ca8068
ufukdogan92 <ufukdogan92@gmail.com>
2017-04-14 13:40:08 +0300

    Add heap instert hafta 7

---
+++ hafta7/heap/index.php
@@ -0,0 +1,42 @@
+<?php
+
+class JupilerLeague extends SplHeap 
+{
+  
+    public function compare($array1, $array2)
+    {
+        $values1 = array_values($array1);
+        $values2 = array_values($array2);
+        if ($values1[0] === $values2[0]) return 0;
+        return $values1[0] < $values2[0] ? -1 : 1;
+    }
+}
+
+// Let's populate our heap here (data of 2009)
+$heap = new JupilerLeague();
+$heap->insert(array ('AA Gent' => 15));
+$heap->insert(array ('Anderlecht' => 20));
+$heap->insert(array ('Cercle Brugge' => 11));
+$heap->insert(array ('Charleroi' => 12));
+$heap->insert(array ('Club Brugge' => 21));
+$heap->insert(array ('G. Beerschot' => 15));
+$heap->insert(array ('Kortrijk' => 10));
+$heap->insert(array ('KV Mechelen' => 18));
+$heap->insert(array ('Lokeren' => 10));
+$heap->insert(array ('Moeskroen' => 7));
+$heap->insert(array ('Racing Genk' => 11));
+$heap->insert(array ('Roeselare' => 6));
+$heap->insert(array ('Standard' => 20));
+$heap->insert(array ('STVV' => 17));
+$heap->insert(array ('Westerlo' => 10));
+$heap->insert(array ('Zulte Waregem' => 15));
+ 
+$heap->top();
+ 
+while ($heap->valid()) {
+  list ($team, $score) = each ($heap->current());
+  echo $team . ': ' . $score . PHP_EOL."</br>";
+  $heap->next();
+}
+
+?>
