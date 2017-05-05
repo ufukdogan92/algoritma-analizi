ad9e95b2ea0dc9d9d9d0597fa7f6defaf8728902
ufukdogan92 <ufukdogan92@gmail.com>
2017-05-05 13:47:01 +0300

    Add hafta10 files - Alt üçgensel matris

--- hafta10/main.c
+++ hafta10/main.c
@@ -5,28 +5,39 @@
 int main()
 {
   int i,j;
-  int a[M][M];  
- 
+  float a[M][M]; 
+  float b[3]={1,2,3}; 
+  float x[3]={1,2,3};
    for(i=0;i<M;i++) 
    {  
     for(j=0;j<M;j++) 
     {             
     printf("a[%d][%d]=", i+1,j+1);
-     scanf("%d",&a[i][j]); 
+     scanf("%f",&a[i][j]); 
     }
    }
-   printf("\nUst Ucgen Matris\n\n");  
+  printf("\n\n"); 
    for(i=0;i<M;i++)
    {
     for(j=0;j<M;j++)
     { 
     if(j>=i)        
-      printf("\t%d\t",a[i][j]);  
+      printf("%f\t",a[i][j]);  
     else printf("\t%d\t",0); 
     }
-    printf("\n"); 
+
+    printf("|\tx%f\t|\t%f\n",x[i],b[i]); 
    }  
    printf("\n");
+   x[2] = b[2]/a[2][2];
+   x[1] = (b[1]/-a[1][2]*x[2])/a[1][1];
+   x[0] = (b[1]/-a[0][2]*x[2]-a[0][1]*x[1])/a[1][1];
+
+   printf("x1=%f \n",x[0]);
+   printf("x2=%f \n",x[1]);
+   printf("x3=%f \n",x[2]);
+    
+
    
   return 0; 
 }
